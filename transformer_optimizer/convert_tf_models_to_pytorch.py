875ac60da6ad81fb39ce2781dd8ba4aeea20f243
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 07:07:56 +0000

    Adding numeric_owner as keyword arguement

--- transformer_optimizer/convert_tf_models_to_pytorch.py
+++ transformer_optimizer/convert_tf_models_to_pytorch.py
@@ -125,7 +125,7 @@ def download_tf_checkpoint(model_name, tf_models_dir="tf_models"):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar_ref, ckpt_dir)
